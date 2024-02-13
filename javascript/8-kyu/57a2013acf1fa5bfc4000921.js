2e47614b31cd1c79e6384555c82d7f7bd4834855
twomodone <twomodone@gmail.com>
2024-02-13 16:03:50 +0200

    Add 57a2013acf1fa5bfc4000921 solution

---
+++ javascript/8-kyu/57a2013acf1fa5bfc4000921.js
@@ -0,0 +1,10 @@
+function findAverage(array) {
+  let length = array.length;
+  if (length === 0){
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < length; i++){
+    sum += array[i];
+  } return sum / length;
+}
