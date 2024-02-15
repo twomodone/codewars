f1f295be1f696270a53d1eb6e6a9205f162d76b3
twomodone <twomodone@gmail.com>
2024-02-15 13:03:31 +0200

    Add 56170e844da7c6f647000063 solution

---
+++ javascript/8-kyu/56170e844da7c6f647000063.js
@@ -0,0 +1,12 @@
+function peopleWithAgeDrink(age) {
+  let drink = "";
+  if (age < 14){
+    drink += "toddy";
+  } else if (age < 18){
+    drink += "coke";
+  } else if (age < 21){
+    drink += "beer";
+  } else {
+    drink += "whisky";
+  } return `drink ${drink}`;
+};
