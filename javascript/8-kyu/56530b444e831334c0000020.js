8c434a0b66e6650d5c0d054a193a325ee4119078
twomodone <twomodone@gmail.com>
2024-02-18 07:21:35 +0200

    Add 56530b444e831334c0000020 solution

---
+++ javascript/8-kyu/56530b444e831334c0000020.js
@@ -0,0 +1,5 @@
+function chromosomeCheck(sperm) {
+  if (sperm.indexOf("Y") !== -1){
+    return "Congratulations! You're going to have a son.";
+  } return "Congratulations! You're going to have a daughter.";
+}
